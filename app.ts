093daa6d9c5bb61a218b1e2c58f5b66940124070
habiba <habibaamasha32@gmail.com>
2025-07-13 14:47:57 +0300

    .

--- app.ts
+++ app.ts
@@ -1,32 +1,32 @@
 import express from "express";
 import { httpLogger, logger } from "./config/pino";
 import { connectDB } from "./config/db";
-
+import paymentRoutes from "./routes/payment.routes";
+import shopRoutes from "./routes/shop.routes";
 import menuItemRoutes from "./routes/menu-item-routes";
-import http from "http";
-import { initSocket } from "./sockets/socketServer";
 import orderRoutes from "./routes/order.routes";
 import categoryRoutes from "./routes/category.routes";
 import userRoutes from "./routes/user.routes";
 import subscriptionRoutes from "./routes/subscription.routes";
 import kitchenRoutes from "./routes/kitchen.routes";
-
-import { languageMiddleware } from "./middlewares/language.middleware";
-
-import paymentRoutes from "./routes/payment.routes";
-import shopRoutes from "./routes/shop.routes";
+import http from "http";
+import { initSocket } from "./sockets/socketServer";
 import { ErrorHandlerMiddleware } from "./middlewares/error-handling.middleware";
+import { languageMiddleware } from "./middlewares/language.middleware";
 import planRoutes from "./routes/plan.routes";
 
+
+
 const app = express();
-const server = http.createServer(app);
-initSocket(server);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(httpLogger);
-
 app.use(languageMiddleware);
+
+const server = http.createServer(app);
+initSocket(server);
 
 app.use("/api/v1/auth/user", userRoutes);
 app.use("/api/v1/subscriptions", subscriptionRoutes);
@@ -34,15 +34,15 @@ app.use("/orders", orderRoutes);
 app.use("/", kitchenRoutes);
 app.use("/menu-items", menuItemRoutes);
 app.use("/category", categoryRoutes);
+app.use("/api/v1/plans", planRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/v1/shops", shopRoutes);
 
 app.use(ErrorHandlerMiddleware);
 
 
 
-app.use("/api/v1/plans", planRoutes);
 
-app.use("/api/payments", paymentRoutes);
-app.use("/api/v1/shops", shopRoutes);
 
 const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
