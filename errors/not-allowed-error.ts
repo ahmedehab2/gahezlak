b909ba011d8d3a40f2415e2cc5fdc47b6771e0af
Ahmed Ehab <ahmede964@gmail.com>
2025-07-15 21:46:04 +0300

    refactor(errors): standardize error messages and language defaults
    - Updated error classes to use consistent formatting for messages and language defaults.
    - Changed default language from 'ar' to 'en' in multiple error classes for better consistency.
    
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

--- errors/not-allowed-error.ts
+++ errors/not-allowed-error.ts
@@ -1,16 +1,15 @@
-import { CustomError } from './abstract-error-class';
-import { ErrorResponse } from '../common/types/contoller-response.types';
-import { LangType, MessageError } from '../common/types/general-types';
-
+import { CustomError } from "./abstract-error-class";
+import { ErrorResponse } from "../common/types/contoller-response.types";
+import { LangType, MessageError } from "../common/types/general-types";
 
 export class NotAllowedError extends CustomError {
   statusCode: number = 405;
   customMessage: MessageError;
   lang: LangType;
   constructor(message?: MessageError, lang?: LangType) {
-    super(message || { en: 'not allowed', ar: 'غير مسموح' }, lang);
-    this.customMessage = message || { en: 'not allowed', ar: 'غير مسموح' };
-    this.lang = lang || 'ar';
+    super(message || { en: "not allowed", ar: "غير مسموح" }, lang);
+    this.customMessage = message || { en: "not allowed", ar: "غير مسموح" };
+    this.lang = lang || "en";
   }
   serializeError(): ErrorResponse {
     const localizedMessage = this.customMessage[this.lang];
--- errors/unprocessable-error.ts
+++ errors/unprocessable-error.ts
@@ -1,16 +1,21 @@
-import { CustomError, } from './abstract-error-class';
-import { ErrorResponse } from '../common/types/contoller-response.types';
-import { LangType, MessageError } from '../common/types/general-types';
-
+import { CustomError } from "./abstract-error-class";
+import { ErrorResponse } from "../common/types/contoller-response.types";
+import { LangType, MessageError } from "../common/types/general-types";
 
 export class UnprocessableError extends CustomError {
   statusCode: number = 406;
   customMessage: MessageError;
   lang: LangType;
   constructor(message?: MessageError, lang?: LangType) {
-    super(message || { en: 'bad request error', ar: 'هناك مشكلة بالطلب' }, lang);
-    this.customMessage = message || { en: 'bad request error', ar: 'هناك مشكلة بالطلب' };
-    this.lang = lang || 'ar';
+    super(
+      message || { en: "bad request error", ar: "هناك مشكلة بالطلب" },
+      lang
+    );
+    this.customMessage = message || {
+      en: "bad request error",
+      ar: "هناك مشكلة بالطلب",
+    };
+    this.lang = lang || "en";
   }
 
   serializeError(): ErrorResponse {
