6d8a32a68cfce5c9f667c97bc96b5603f6c193a5
Ahmed Ehab <ahmede964@gmail.com>
2025-07-18 17:19:19 +0300

    change multer middleware to older version
    Signed-off-by: Ahmed Ehab <ahmede964@gmail.com>

--- middlewares/multer.ts
+++ middlewares/multer.ts
@@ -5,17 +5,18 @@ const storage = multer.memoryStorage();
 const limits = { fileSize: 1 * 1024 * 1024 }; // 1MB limit
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb: Function) => {
-  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/webp'];
-  
-  if (!allowedMimeTypes.includes(file.mimetype)) {
-    return cb(new Error('Only JPEG, PNG, and WebP images are allowed'));
+  if (!file.mimetype.startsWith("image/")) {
+    return cb(new Error("Only image files are allowed"));
   }
-  
   cb(null, true);
 };
 
-export const uploadMiddleware = multer({
-  storage,
-  limits,
-  fileFilter,
-}).single('image'); 
+function uploadMiddleware(fileName: string) {
+  return multer({
+    storage,
+    limits,
+    fileFilter,
+  }).single(fileName);
+}
+
+export { uploadMiddleware };
